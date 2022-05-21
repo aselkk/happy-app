5ae078b6d687a2236bd8507a0d9ec4949673214e
“Asel” <aselkurmanbekk@gmail.com>
2022-05-21 15:20:10 +0600

    add dark mode toggle

---
+++ src/js/darkmode-section.js
@@ -0,0 +1,25 @@
+let icon = document.querySelector('.darkmode-section__toggle-bttns__btn-container__btn-off')
+let iconTwo = document.querySelector('.darkmode-section__toggle-bttns__btn__on')
+
+    icon.addEventListener("click", function(){ 
+        document.body.classList.toggle("light-mode")
+        if(document.body.classList.contains("light-mode")){
+            icon.src='/src/assets/img/light-mode-on.svg'
+            iconTwo.src='/src/assets/img/dark-mode-off.svg'
+        } else {
+            icon.src='/src/assets/img/light-mode-off.svg'
+            iconTwo.src='/src/assets/img/dark-mode-on.svg'
+        }
+    });
+    iconTwo.addEventListener("click", function(){ 
+        document.body.classList.toggle("light-mode")
+        if(document.body.classList.contains("light-mode")){
+            icon.src='/src/assets/img/light-mode-on.svg'
+            iconTwo.src='/src/assets/img/dark-mode-off.svg'
+        }
+        else {
+            icon.src='/src/assets/img/light-mode-off.svg'
+            iconTwo.src='/src/assets/img/dark-mode-on.svg'
+        }
+    });
+
