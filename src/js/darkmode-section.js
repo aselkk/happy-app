b9d24b8c666b60d86520f2d8ecd6517b8f219824
“Asel” <aselkurmanbekk@gmail.com>
2022-05-24 15:58:54 +0600

    move icons to separate folder

--- src/js/darkmode-section.js
+++ src/js/darkmode-section.js
@@ -1,34 +1,7 @@
-let icon = document.querySelector('.darkmode-section__toggle-bttns__btn-container__btn__off')
-let iconTwo = document.querySelector('.darkmode-section__toggle-bttns__btn-container__btn__on')
-let text = document.querySelectorAll('.dark-mode-toggle-text')
-let darkModeSection = document.querySelector('.darkmode-section')
-
-    // toggle = function(){
-    //     document.body.classList.toggle("light-mode")
-    //     icon.classList.toggle("light-mode")
-    //     if(document.body.classList.contains("light-mode")){
-    //         iconTwo.classList.toggle("light-mode")
-    //         text.innerHTML='light'
-    //     } else {
-    //         text.innerHTML='dark'
-    //     }
-    // }
-    // icon.addEventListener("click", toggle);
-    // iconTwo.addEventListener("click", toggle);
-
-    // const lightMode = document.querySelector('.light__btn');
-    // const darkMode = document.querySelector('.dark__btn');
-
-    // lightMode.addEventListener("click", function(e){
-    //     darkModeSection.classList.toggle("light-mode")
-    //     text.innerHTML='dark'
-    // })
-    // darkMode.addEventListener("click", function(e){
-    //     darkModeSection.classList.toggle("dark-mode")
-    //     text.innerHTML='light'
-    // })
-    const lightBtn = document.getElementById("light__btn")
-    const darkBtn = document.getElementById("dark__btn")
+const text = document.querySelectorAll('.dark-mode-toggle-text')
+const darkModeSection = document.querySelector('.darkmode-section')
+const darkBtn = document.getElementById("dark__btn")
+const lightBtn = document.getElementById("light__btn")
 
     lightBtn.addEventListener("click", function(){
         darkModeSection.classList.remove("dark-mode")
@@ -36,8 +9,6 @@ let darkModeSection = document.querySelector('.darkmode-section')
         lightBtn.classList.add('light-mode-on');
         darkBtn.classList.remove('light-mode-on');
         text.forEach(e => e.innerHTML='light')
-        
-
     })
     darkBtn.addEventListener("click", function(){
         lightBtn.classList.remove('light-mode-on')
