fc3164229fed94795ff46ee470a753f16cfa0136
“Asel” <aselkurmanbekk@gmail.com>
2022-05-24 15:31:20 +0600

    complete dark/light mode functionality

--- src/js/darkmode-section.js
+++ src/js/darkmode-section.js
@@ -1,27 +1,53 @@
-let icon = document.querySelector('.darkmode-section__toggle-bttns__btn-container__btn-off')
-let iconTwo = document.querySelector('.darkmode-section__toggle-bttns__btn__on')
+let icon = document.querySelector('.darkmode-section__toggle-bttns__btn-container__btn__off')
+let iconTwo = document.querySelector('.darkmode-section__toggle-bttns__btn-container__btn__on')
+let text = document.querySelectorAll('.dark-mode-toggle-text')
+let darkModeSection = document.querySelector('.darkmode-section')
 
-    icon.addEventListener("click", function(){ 
-        document.body.classList.toggle("light-mode")
-        if(document.body.classList.contains("light-mode")){
-            icon.src='/src/assets/img/light-mode-on.svg'
-            iconTwo.src='/src/assets/img/dark-mode-off.svg'
-        } else {
-            icon.src='/src/assets/img/light-mode-off.svg'
-            iconTwo.src='/src/assets/img/dark-mode-on.svg'
-        }
-    });
-    iconTwo.addEventListener("click", function(){ 
-        document.body.classList.toggle("light-mode")
-        document.querySelector('.darkmode-section__toggle-bttns__text__off').toggle('light-mode-text')
-        if(document.body.classList.contains("light-mode")){
-            icon.src='/src/assets/img/light-mode-on.svg'
-            iconTwo.src='/src/assets/img/dark-mode-off.svg'
-        }
-        else {
-            icon.src='/src/assets/img/light-mode-off.svg'
-            iconTwo.src='/src/assets/img/dark-mode-on.svg'
-        }
-    });
+    // toggle = function(){
+    //     document.body.classList.toggle("light-mode")
+    //     icon.classList.toggle("light-mode")
+    //     if(document.body.classList.contains("light-mode")){
+    //         iconTwo.classList.toggle("light-mode")
+    //         text.innerHTML='light'
+    //     } else {
+    //         text.innerHTML='dark'
+    //     }
+    // }
+    // icon.addEventListener("click", toggle);
+    // iconTwo.addEventListener("click", toggle);
+
+    // const lightMode = document.querySelector('.light__btn');
+    // const darkMode = document.querySelector('.dark__btn');
+
+    // lightMode.addEventListener("click", function(e){
+    //     darkModeSection.classList.toggle("light-mode")
+    //     text.innerHTML='dark'
+    // })
+    // darkMode.addEventListener("click", function(e){
+    //     darkModeSection.classList.toggle("dark-mode")
+    //     text.innerHTML='light'
+    // })
+    const lightBtn = document.getElementById("light__btn")
+    const darkBtn = document.getElementById("dark__btn")
+
+    lightBtn.addEventListener("click", function(){
+        darkModeSection.classList.remove("dark-mode")
+        darkModeSection.classList.add("light-mode")
+        lightBtn.classList.add('light-mode-on');
+        darkBtn.classList.remove('light-mode-on');
+        text.forEach(e => e.innerHTML='light')
+        
+
+    })
+    darkBtn.addEventListener("click", function(){
+        lightBtn.classList.remove('light-mode-on')
+        darkModeSection.classList.remove("light-mode")
+        darkBtn.classList.add('light-mode-on');
+        lightBtn.classList.remove('light-mode-on');
+        text.forEach(e => e.innerHTML='dark')
+    })
+
+
+
 
 
